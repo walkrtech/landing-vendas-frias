68b51f2da2d587de43022c4fa525d071d196b958
Aenã <aenamartinelli@gmail.com>
2022-10-05 14:47:50 -0300

    new_video

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,32 @@
+const video = document.getElementById('video');
+
+const btn = document.getElementById('btn-video');
+const btnRestart = document.getElementById('btn-restart');
+
+const imageButton = document.getElementById('image-button');
+
+let width = 0;
+
 window.addEventListener('scroll', () => {
     document.querySelectorAll('.reveal').forEach(reveal => {
         let windowHeight = window.innerHeight;
         let revealTop = reveal.getBoundingClientRect().top;
 
-        if(revealTop < windowHeight - 200){
+        if (revealTop < windowHeight - 200) {
             reveal.classList.add('slidein')
-        }else{
+        } else {
             reveal.classList.remove('slidein')
         }
     })
 })
 
 function display(module) {
-    
+
     document.querySelectorAll(`.module-${module}`).forEach(element => {
-        if(element.classList.contains("mobile")){
+        if (element.classList.contains("mobile")) {
             element.classList.remove("mobile")
             document.getElementById(`arrow-${module}`).setAttribute("src", "https://landing-vendas.s3.amazonaws.com/icons/simple-arrow-down.svg")
-        }else {
+        } else {
             document.getElementById(`arrow-${module}`).setAttribute("src", "https://landing-vendas.s3.amazonaws.com/icons/simple-arrow-up.svg")
             element.classList.add("mobile")
         }
@@ -27,11 +36,37 @@ function display(module) {
 function showFaq(btn) {
     let icon = event.srcElement.textContent
 
-    if(icon == "+"){
+    if (icon == "+") {
         document.getElementById(`btn-faq-${btn}`).classList.remove('d-none')
         event.srcElement.innerHTML = "-"
-    }else{
+    } else {
         document.getElementById(`btn-faq-${btn}`).classList.add('d-none')
         event.srcElement.innerHTML = "+"
     }
 }
+
+
+function videoStatus() {
+    let duration = ((video.duration / 60) * 100).toFixed(0);
+    if (video.paused) {
+        video.play();
+        imageButton.src = 'https://landing-vendas.s3.amazonaws.com/landing-fria/icons/pause.svg'
+        if (width == 0) {
+            document.documentElement.style.setProperty('--animation-status', "running");
+            document.documentElement.style.setProperty('--video-duration', duration + 's');
+        }
+    } else {
+        video.pause();
+        imageButton.src = 'https://landing-vendas.s3.amazonaws.com/landing-fria/icons/play.svg'
+        document.documentElement.style.setProperty('--animation-status', "paused");
+    }
+}
+
+function restartVideo() {
+    video.load()
+}
+
+video.addEventListener('ended', () => {
+    btn.style.display = 'none'
+    btnRestart.style.display = 'unset'
+})
